b6727df5e2b077aee887207a4189575ef1cede20
0RAPTOR0 <xxiiraptor@gamil.com>
2024-05-23 19:23:10 +0530

    loops

--- loops.js
+++ loops.js
@@ -14,3 +14,26 @@ for( let i=240; i<=370; i++)
     if(i % 7 === 0 && i % 11 === 0 ){
         console.log(i);
     }
+
+//    while loop
+    let num = 10
+
+    while(num < 20){
+        console.log(num);
+        num+=2
+    }
+
+    // dowhile loop
+    let a = 15;
+
+    do{
+        console.log(a);
+     }while(a < 10)
+
+        // for each loop
+        let nums = [ 23, 45, 92, 54, 78, 37 ];
+        // array
+
+        for( let n of nums){
+            console.log(n);
+        }
